405bfea08de7b1c8a7196001d27926de8239217b
Illera1999 <illerarodriguez1999@gmail.com>
2024-11-17 20:23:52 +0000

    Try Catch y Finally

--- introduction_dart/futuros.dart
+++ introduction_dart/futuros.dart
@@ -1,25 +1,25 @@
-void main(List<String> args) {
+void main() async {
   print('Inicio del programa') ;
 
-  httpGet('https://www.prueba.com')
-  .then((value) {
-    print(value);
-  })
-  .catchError((error){
+  try{
+    final valute = await httpGet('https://www.api.com/valute');
+    print(valute);
+  } on Exception catch(error) {
+    print('Tenemos una excepción');
+    print(error);
+  }catch(error) {
     print('Error: $error');
-
-  });
-
+  } finally {
+    print('Fin del try y el catch');
+  }
   print('Fin del programa') ;
 }
 
 
 
 Future<String> httpGet(String url) async {
-
-  return Future.delayed(Duration(seconds: 1), () {
-    throw Exception('Error en la petición http');
-    // return 'Respuesta de la petición http';
-  });
-
+  await Future.delayed(Duration(seconds: 1));
+  throw Exception('No hay parámetros en la URL');
+  // throw Exception('Error en la conexión');
+  // return 'Respuesta del servidor';
 }
